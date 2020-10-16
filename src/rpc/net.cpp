bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Split CConnman::GetNodeCount() in two functions
This change prevents repetitive traversing vNodes.

src/rpc/net.cpp
@@ -47,7 +47,7 @@ static UniValue getconnectioncount(const JSONRPCRequest& request)
     if(!node.connman)
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
 
-    return (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_ALL);
+    return (int)node.connman->PeerCount();
 }
 
 static UniValue ping(const JSONRPCRequest& request)
@@ -540,9 +540,10 @@ static UniValue getnetworkinfo(const JSONRPCRequest& request)
     obj.pushKV("timeoffset",    GetTimeOffset());
     if (node.connman) {
         obj.pushKV("networkactive", node.connman->GetNetworkActive());
-        obj.pushKV("connections", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_ALL));
-        obj.pushKV("connections_in", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_IN));
-        obj.pushKV("connections_out", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_OUT));
+        const ConnectionCounts conn_counts = node.connman->ConnectionCounts();
+        obj.pushKV("connections", conn_counts.all);
+        obj.pushKV("connections_in", conn_counts.in);
+        obj.pushKV("connections_out", conn_counts.out);
     }
     obj.pushKV("networks",      GetNetworksInfo());
     obj.pushKV("relayfee",      ValueFromAmount(::minRelayTxFee.GetFeePerK()));

bitcoin-core/gui

src/rpc/net.cpp
@@ -56,7 +56,7 @@ static RPCHelpMan getconnectioncount()
     if(!node.connman)
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
 
-    return (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_ALL);
+    return (int)node.connman->PeerCount();
 },
     };
 }
@@ -583,9 +583,10 @@ static RPCHelpMan getnetworkinfo()
     obj.pushKV("timeoffset",    GetTimeOffset());
     if (node.connman) {
         obj.pushKV("networkactive", node.connman->GetNetworkActive());
-        obj.pushKV("connections", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_ALL));
-        obj.pushKV("connections_in", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_IN));
-        obj.pushKV("connections_out", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_OUT));
+        const ConnCounts conn_counts = node.connman->ConnectionCounts();
+        obj.pushKV("connections", conn_counts.all);
+        obj.pushKV("connections_in", conn_counts.in);
+        obj.pushKV("connections_out", conn_counts.out);
     }
     obj.pushKV("networks",      GetNetworksInfo());
     obj.pushKV("relayfee",      ValueFromAmount(::minRelayTxFee.GetFeePerK()));