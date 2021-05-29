bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

qt, refactor: Make BitcoinUnits::Unit a scoped enum

src/qt/optionsmodel.cpp
@@ -512,7 +512,7 @@ void OptionsModel::setDisplayUnit(const QVariant &value)
         QSettings settings;
         m_display_unit = value.value<BitcoinUnit>();
         settings.setValue("display_unit", QVariant::fromValue(m_display_unit));
-        Q_EMIT displayUnitChanged(static_cast<int>(m_display_unit));
+        Q_EMIT displayUnitChanged(m_display_unit);
     }
 }
 

bitcoin-core/gui

src/qt/optionsmodel.cpp
@@ -526,7 +526,7 @@ void OptionsModel::setDisplayUnit(const QVariant &value)
         QSettings settings;
         m_display_bitcoin_unit = value.value<BitcoinUnit>();
         settings.setValue("DisplayBitcoinUnit", QVariant::fromValue(m_display_bitcoin_unit));
-        Q_EMIT displayUnitChanged(static_cast<int>(m_display_bitcoin_unit));
+        Q_EMIT displayUnitChanged(m_display_bitcoin_unit);
     }
 }
 