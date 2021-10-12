rebroad/bitcoin

Display send/recv in bps instead of totals in the debug window

src/qt/guiutil.h
@@ -238,6 +238,7 @@ namespace GUIUtil
 
     QString formatNiceTimeOffset(qint64 secs);
 
+    QString formatBps(uint64_t bytes);
     QString formatBytes(uint64_t bytes);
 
     qreal calculateIdealFontSize(int width, const QString& text, QFont font, qreal minPointSize = 4, qreal startPointSize = 14);

bitcoin-core/gui

src/qt/guiutil.h
@@ -229,6 +229,7 @@ namespace GUIUtil
     QString formatNiceTimeOffset(qint64 secs);
 
     QString formatBytes(uint64_t bytes);
+    QString formatBps(float bits);
 
     qreal calculateIdealFontSize(int width, const QString& text, QFont font, qreal minPointSize = 4, qreal startPointSize = 14);
 