a6f567590b73986fde83d8face6827e0801a2e8c
John Moffett <john.moff@gmail.com>
2022-12-02 11:06:43 -0500

    Fix Requested Payments History Multiselect
    The recent payment requests list has somewhat broken functionality. You can only
    select contiguous rows. Sorting the list loses the selection data. The context
    menu appears when multiple rows are selected despite the actions only affecting
    the first in the list. These issues are all corrected.
    
    First, a QSortFilterProxyModel is inserted to take care of sorting instead of sorting
    the model manually. This also preserves selection data when sorting. Second, the
    selection model is changed to ExtendedSelection to allow for non-contiguous multi-
    select. Third, the context menu now operates on multiple selections properly. It will
    copy newline-separated rows of data if appropriate.

--- src/qt/receivecoinsdialog.h
+++ src/qt/receivecoinsdialog.h
@@ -13,6 +13,7 @@
 #include <QKeyEvent>
 #include <QMenu>
 #include <QPoint>
+#include <QSortFilterProxyModel>
 #include <QVariant>
 
 class PlatformStyle;
@@ -51,15 +52,20 @@ public Q_SLOTS:
 
 private:
     Ui::ReceiveCoinsDialog *ui;
+    QSortFilterProxyModel* m_sort_proxy;
     WalletModel* model{nullptr};
     QMenu *contextMenu;
+    QAction* copyURIAction;
+    QAction* copyAddressAction;
     QAction* copyLabelAction;
     QAction* copyMessageAction;
     QAction* copyAmountAction;
     const PlatformStyle *platformStyle;
 
-    QModelIndex selectedRow();
+    // Returns QModelIndex in source model
+    QModelIndexList SelectedRows();
     void copyColumnToClipboard(int column);
+    void ShowReceiveRequestDialogForItem(const QModelIndex& index);
 
 private Q_SLOTS:
     void on_receiveButton_clicked();
