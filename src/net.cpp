rebroad/bitcoin

net: Add setTotalBytes functionality for traffic data persistence
Add functions to set total bytes sent/received counts to enable restoring
traffic statistics between application sessions. This allows for continuous
traffic monitoring even after application restarts, improving the usefulness
of the traffic graph widget.

src/net.cpp
@@ -2979,6 +2979,14 @@ uint64_t CConnman::GetTotalBytesSent() const
     return nTotalBytesSent;
 }
 
+void CConnman::SetTotalBytesRecv(uint64_t bytes) { nTotalBytesRecv.store(bytes); }
+
+void CConnman::SetTotalBytesSent(uint64_t bytes)
+{
+    LOCK(cs_totalBytesSent);
+    nTotalBytesSent = bytes;
+}
+
 ServiceFlags CConnman::GetLocalServices() const
 {
     return nLocalServices;

bitcoin-core/gui

src/net.cpp
@@ -3779,6 +3779,14 @@ uint64_t CConnman::GetTotalBytesSent() const
     return nTotalBytesSent;
 }
 
+void CConnman::SetTotalBytesRecv(uint64_t bytes) { nTotalBytesRecv.store(bytes); }
+
+void CConnman::SetTotalBytesSent(uint64_t bytes)
+{
+    LOCK(m_total_bytes_sent_mutex);
+    nTotalBytesSent = bytes;
+}
+
 ServiceFlags CConnman::GetLocalServices() const
 {
     return m_local_services;