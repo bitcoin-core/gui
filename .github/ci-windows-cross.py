faf738946668b6ec16de37298e5a1da23ed77222
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2026-02-10 11:20:02 +0100

    ci: Move check_manifests step to ci-windows-cross.py
    This is almost a refactor. The only change is putting the
    bitcoind.manifest into a different folder.

--- .github/ci-windows-cross.py
+++ .github/ci-windows-cross.py
@@ -24,16 +24,47 @@ def print_version():
     run([str(bitcoind), "-version"])
 
 
+def check_manifests():
+    release_dir = Path.cwd() / "bin"
+    manifest_path = release_dir / "bitcoind.manifest"
+
+    cmd_bitcoind_manifest = [
+        "mt.exe",
+        "-nologo",
+        f"-inputresource:{release_dir / 'bitcoind.exe'}",
+        f"-out:{manifest_path}",
+    ]
+    run(cmd_bitcoind_manifest)
+    print(manifest_path.read_text())
+
+    skipped = {  # Skip as they currently do not have manifests
+        "fuzz.exe",
+        "bench_bitcoin.exe",
+        "test_kernel.exe",
+    }
+    for entry in release_dir.iterdir():
+        if entry.suffix.lower() != ".exe":
+            continue
+        if entry.name in skipped:
+            print(f"Skipping {entry.name} (no manifest present)")
+            continue
+        print(f"Checking {entry.name}")
+        run(["mt.exe", "-nologo", f"-inputresource:{entry}", "-validate_manifest"])
+
+
 def main():
     parser = argparse.ArgumentParser(description="Utility to run Windows CI steps.")
     steps = [
         "print_version",
+        "check_manifests",
     ]
     parser.add_argument("step", choices=steps, help="CI step to perform.")
     args = parser.parse_args()
 
     if args.step == "print_version":
         print_version()
+    elif args.step == "check_manifests":
+        check_manifests()
 
 
 if __name__ == "__main__":
